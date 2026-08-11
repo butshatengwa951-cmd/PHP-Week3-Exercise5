9e48186a9d264efacc3189d3f1510eb6f47d62aa
butshatengwa951-cmd <butshatengwa951@gmail.com>
2026-08-11 10:10:07 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "my_app");
+$conn = new mysqli("localhost:3307", "root", "Butsha06#", "my_app");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
